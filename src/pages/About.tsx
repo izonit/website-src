062fedaaef6cc8b0d37c443c4662ddb5e5523858
Mizorint <148252867+izonit@users.noreply.github.com>
2024-12-29 17:11:39 +0400

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -14,8 +14,7 @@ export default function About() {
       <div className="center">
         <h1 className="title">About</h1>
       </div>
-      <h2>I am Mizorint (but you know this already), and my real name is Daniil. And I am an amateur web developer! I like writing ugly code in TypeScript.</h2>
-      <h2>Currently, I am not succeeding much in my coding journey. But that will certainly be fixed.</h2>
+      <h2>Nothing much about me. Just keep in mind that I am a beginner.</h2>
       <h3>Oh, and right <Link to="/contacts" className='link'>here</Link> you can contact me!</h3>
     </>
   );
